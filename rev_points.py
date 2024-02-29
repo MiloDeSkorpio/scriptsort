f6385efed3868e7c4364072fdbea82f4644ad52e
MiloDeSkorpio <arcadio.2es0@gmail.com>
2024-02-29 14:11:57 -0600

    Smarpos_rev_mensual

---
+++ rev_points.py
@@ -0,0 +1,63 @@
+##importar librearias necesarias para el funcionamiento del script
+import os
+import pandas as pd
+
+## 
+smartpos = 'SMARTPOS1494126447'
+## Nombre del mes con texto, se ocupara para leer la carpeta del mes y asignar el nombre a los archivos generados
+mes_nombre = "Enero"
+
+## Modificar el contenido de m = "mes" * Para los meses que anteriores a octubre ocupar la sintaxis 09 = Septiembre 08 = Agosto
+## Modificar el contenido de Y = "Año" 2023 / 2024 / 2025 
+m = "01"
+y = "2024"
+
+## Nombre de las extenciones de los archivos que ocupara el script para realizar 
+a = "-Transacciones.csv"
+ae = "-Transacciones-extension.csv"
+
+## Ruta de la cual se extraeran todos los archivos y en la misma se guardaran los archivos
+ruta_guardado = f"Transacciones/{y}/{m} {mes_nombre}"
+
+## Este es el rango de dias en el que se trabajara, para el tema del ultimo dia siempre se le sumara 1
+## Ejemplo primera quincena dia_fn = 16 el metodo range trabaja de esa forma
+dia_in = 1
+dia_fn = 32
+rango = dia_fn - dia_in
+
+## Listado de los archvios -Transacciones.csv
+## Listado de los archivo a leer segun el rango especificado 
+archivo_tr = [os.path.join(ruta_guardado, f"{y}{m}{d:02d}{a}") for d in range(dia_in, dia_fn)]
+
+## Leer Archivos de Extencion para obtener la duracion de las transacciones
+## Lista de nombres de archivo
+archivo_ex = [os.path.join(ruta_guardado, f"{y}{m}{d:02d}{ae}") for d in range(dia_in, dia_fn)]
+
+## Areglo que se llenara con los archivos -Transacciones-extension.csv
+transacciones = []
+## Bucle para insertar todos los archivos en el DataFrame transacciones
+for transaccion in archivo_tr:
+  df = pd.read_csv(transaccion, low_memory=False)
+  transacciones.append(df)
+
+## Concatenación de documentos extraídos del arreglo transacciones y creando un solo DataFrame con información de toda la quincena
+## Arreglo que se llenara con los archivos -Transacciones.csv
+extenciones = []
+## Bucle para insertar todos los archivos en el DataFrame extenciones
+for extencion in archivo_ex:
+  df = pd.read_csv(extencion)
+  extenciones.append(df)
+
+df_smartpos = pd.concat(extenciones,ignore_index=True)
+df_trmes = pd.concat(transacciones,ignore_index=True)
+
+smartpos_filtered = df_smartpos[df_smartpos['DEVICE_ID'] == smartpos]
+df_coincidencias = pd.merge(df_trmes, smartpos_filtered, on="ID_TRANSACCION_ORGANISMO", how="inner")
+
+archivo_mens = f"{smartpos}_{mes_nombre}.csv"
+ruta_res_mens = os.path.join(ruta_guardado, archivo_mens)
+df_coincidencias.to_csv(ruta_res_mens, index=False)
+print('Analisis Finalizado con Exito!!')
+
+
+
