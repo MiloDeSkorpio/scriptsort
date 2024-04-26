1c46ab6a8361aab13cb4c3c7163405cf61d29cd9
MiloDeSkorpio <arcadio.2es0@gmail.com>
2024-04-25 18:09:27 -0600

    InsertCSV_Success

--- postgre_insert_csv.py
+++ postgre_insert_csv.py
@@ -4,13 +4,13 @@ import os
 
 ##
 ## nombre de la tabla
-mes_nombre = "Marzo"
-m = "03"
-y = "2022"
+mes_nombre = "Abril"
+m = "04"
+y = "2024"
 tabla = f"datos_{y}"
-root = 'Transacciones'
+root = 'Validadores'
 ruta_trabajo = f'{root}/{y}/{m} {mes_nombre}'
-name_file = "Marzo_crudo.csv"
+name_file = f"Mpeso_1ra qna Abril.csv"
 # Parámetros de conexion
 connection_string = {
     "host": "localhost",
@@ -20,57 +20,41 @@ connection_string = {
     "port": 5432,
 }
 
-# Función para manejar la conexión a la base de datos
-def connect_to_db(connection_string):
-    try:
-        return psycopg2.connect(**connection_string)
-    except Exception as e:
-        print(f"Error al conectar a la base de datos: {e}")
-        return None
+connection = psycopg2.connect(**connection_string)
 
 # Función para cargar los datos del DataFrame a la tabla sin conversiones
 def load_data_to_table_direct(df, table_name, connection):
     try:
-        # Handling missing values (example: fill with 0)
         default_date = '2000-01-01'
         df['CONTRACT_VALIDITY_START_DATE'] = df['CONTRACT_VALIDITY_START_DATE'].fillna(default_date)
-        df = df.fillna(0)  # Adjust as neede
+        # Replace 'Invalid date' with '2000-01-01' where the value is 'Invalid date'
+        df.loc[df['CONTRACT_VALIDITY_START_DATE'] == 'Invalid date', 'CONTRACT_VALIDITY_START_DATE'] = default_date
+
+        df['FECHA_HORA_TRANSACCION'] = df['FECHA_HORA_TRANSACCION'].fillna(default_date)
+        df = df.fillna(0) 
         # Obtener nombres de las columnas
         columns = list(df.columns)
         
         lowercase_columns = [name.lower() for name in columns]
-        # print(lowercase_columns)
-        # Ejecutar la consulta INSERT
         cursor = connection.cursor()
-        ##
         datos = df.values
-        ##
+
         querry = f"""
             INSERT INTO {table_name} ({", ".join(lowercase_columns)})
             VALUES ({", ".join(["%s"] * len(lowercase_columns))})
-             
-            ;
         """
-        # print(querry)
         cursor.executemany(querry,datos.tolist())
         connection.commit()
         cursor.close()
-        print(f"Se han cargado {df.shape[0]} filas a la tabla {table_name}")
+        print(f"Se han cargado {df.shape[0]} filas a la tabla {table_name} del {mes_nombre}")
     except Exception as e:
         print(e.__class__.__name__, ":", e)
 
-
-# Leer archivo CSV
-
 ruta_archivo = os.path.join(ruta_trabajo,name_file)
-df = pd.read_csv(ruta_archivo)
-# print(df.columns)
-# Conectar a la base de datos
-connection = connect_to_db(connection_string)
-print(df['FECHA_HORA_TRANSACCION'])
-# Cargar datos a la tabla sin conversiones
+df = pd.read_csv(ruta_archivo,encoding='latin-1',low_memory=False)
+
 if connection:
     print("Conexión exitosa")
-    print("Llenando tabla de Recargas ...")
+    print(f"Llenando tabla {tabla} ...")
     load_data_to_table_direct(df, tabla, connection)
 
