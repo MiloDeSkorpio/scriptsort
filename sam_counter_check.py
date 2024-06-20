f35a400b673b0ca5c1db6410d220bf6de468674f
MiloDeSkorpio <arcadio.2es0@gmail.com>
2024-06-19 18:31:31 -0600

    NewFormat

--- sam_counter_check.py
+++ sam_counter_check.py
@@ -16,17 +16,32 @@ archivo = f'Full_{mes}.csv'
 archivo = os.path.join(ruta_trabajo, archivo)
 # Lectura del archivo de Entrada
 df = pd.read_csv(archivo, low_memory=False, encoding='latin-1')
-
-# print(len(df['SAM_SERIAL_HEX'].unique().tolist()))
-# print(df['SAM_SERIAL_HEX'].unique().tolist())
+df['TIPO_TRANSACCION'] = df['TIPO_TRANSACCION'].astype('str')
 lista_sams = df['SAM_SERIAL_HEX'].unique().tolist()
 lista_sams = sorted(lista_sams)
 
 lista = f'Reporte_SAM´s_{mes}.xlsx'
 ruta_lista = os.path.join(ruta_trabajo,lista)
+res = []
 with pd.ExcelWriter(ruta_lista) as writer:
   for sam in lista_sams:
-      df_sam = df[df['SAM_SERIAL_HEX'] == sam]
-      df_sam_ordenado = df_sam[['ID_TRANSACCION_ORGANISMO', 'CONTADOR_RECARGAS']].sort_values(by='CONTADOR_RECARGAS', ascending=True)
-      df_sam_ordenado.to_excel(writer, index=False ,sheet_name=f'{sam}') 
+    df_sam = df[df['SAM_SERIAL_HEX'] == sam]
+    acre = len(df_sam[df_sam['TIPO_TRANSACCION'] == '0'])
+    print(acre)
+    errors = len(df_sam[df_sam['TIPO_TRANSACCION'] == 'D0'])
+    print(errors)
+    reint = len(df_sam[df_sam['TIPO_TRANSACCION'] == '50'])
+    print(reint)
+    res.append({
+      'SAM': sam,
+      'Acreditadas': acre,
+      'Errores': errors,
+      'Reintentos': reint,
+      'Saltos': '',
+      'Total':''
+    })
+    df_res = pd.DataFrame(res) 
+    df_sam_ordenado = df_sam[['ID_TRANSACCION_ORGANISMO', 'CONTADOR_RECARGAS']].sort_values(by='CONTADOR_RECARGAS', ascending=True)
+    df_res.to_excel(writer, index=False ,sheet_name=f'RESUMEN_{mes}') 
+    df_sam_ordenado.to_excel(writer, index=False ,sheet_name=f'{sam}') 
           
